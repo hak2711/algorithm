514127180d55215645ba7488dd80456e6999cb67
hak2711 <hak271111@gmail.com>
2021-04-26 10:23:40 +0900

    210426 정렬

---
+++ BOJ/11650_좌표 정렬하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    vector<pair<int, int>> v;
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+        v.push_back(make_pair(x, y));
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < N; i++) {
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+}
