18013ab140fa038dd6b68676a1f25cda87867a4f
hak2711 <hak271111@gmail.com>
2021-04-27 11:01:20 +0900

    210427 입출력 기본 c++

---
+++ BOJ/10000/10953_A+B-6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        scanf("%d,%d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
