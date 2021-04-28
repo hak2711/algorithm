12d3ae0badff4fceddb2acaae97f157e546d2f61
hak2711 <hak271111@gmail.com>
2021-04-28 11:07:50 +0900

    210428 입출력 기본 py

--- BOJ/11000/11022_A+B-8.py
+++ BOJ/11000/11022_A+B-8.py
@@ -1,6 +1,5 @@
-import sys
+t = int(input())
 
-while(True):
-    try:
-        s = sys.stdin.readline()
-        print(s)
+for i in range(t):
+    a,b = map(int, input().split())
+    print("Case #{0}: {1} + {2} = {3}".format(i+1,a,b,a+b))
