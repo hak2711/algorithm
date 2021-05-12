28bb3d7d700f79c5f7cb073a2dd4a886cfb36a17
hak2711 <hak271111@gmail.com>
2021-05-12 11:33:58 +0900

    코드 백업 sort cpp

--- BOJ/11000/11650_좌표정렬하기.cpp
+++ BOJ/11000/11650_좌표정렬하기.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     vector<pair<int, int>> v;
     int N;
 
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         int x, y;
         cin >> x >> y;
         v.push_back(make_pair(x, y));
@@ -18,7 +20,8 @@ int main(void) {
 
     sort(v.begin(), v.end());
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         cout << v[i].first << " " << v[i].second << "\n";
     }
 }
