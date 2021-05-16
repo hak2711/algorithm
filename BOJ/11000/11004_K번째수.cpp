30d3480f96a22d83feb473fb72bf80d9d975488a
hak2711 <hak271111@gmail.com>
2021-05-16 21:09:11 +0900

    210516 sort cpp

---
+++ BOJ/11000/11004_K번째수.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <algorithm>
+#define MAX 5000001
+
+int nums[MAX];
+int sorted[MAX];
+
+void merge(int left, int mid, int right)
+{
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+
+    while (i <= mid && j <= right)
+    {
+        if (nums[i] <= nums[j])
+            sorted[k++] = nums[i++];
+        else
+            sorted[k++] = nums[j++];
+    }
+
+    if (i > mid)
+    {
+        for (int w = j; w <= right; w++)
+        {
+            sorted[k++] = nums[j++];
+        }
+    }
+    else
+    {
+        for (int w = i; w <= mid; w++)
+        {
+            sorted[k++] = nums[i++];
+        }
+    }
+
+    for (int w = left; w <= right; w++)
+        nums[w] = sorted[w];
+}
+
+void mergeSort(int left, int right)
+{
+    int mid;
+    if (left < right)
+    {
+        mid = (left + right) / 2;
+        mergeSort(left, mid);
+        mergeSort(mid + 1, right);
+        merge(left, mid, right);
+    }
+}
+
+int main(void)
+{
+    int k, n;
+    scanf("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    mergeSort(0, n - 1);
+
+    printf("%d", nums[k - 1]);
+}
